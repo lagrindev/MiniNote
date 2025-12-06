35f62ac60895635f67865703868c53bf4d64bc78
LagrinDev <45242069+lagrindev@users.noreply.github.com>
2025-12-06 18:27:22 +0300

    add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
 }
