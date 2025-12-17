2c937043ff58d4167c23486aa3ccd6b7b59283ca
LagrinDev <45242069+lagrindev@users.noreply.github.com>
2025-12-17 22:22:07 +0300

    Update MainActivity.kt

--- app/src/main/java/ru/lagrindev/mininote/MainActivity.kt
+++ app/src/main/java/ru/lagrindev/mininote/MainActivity.kt
@@ -12,9 +12,7 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.room.Room
 
